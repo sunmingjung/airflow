30ae6a2abc899dc11b34b948ccb4fe4ed6298567
sunmingjung <tjsal7454@gmail.com>
2024-03-22 06:28:40 -0700

    simple http operator dag 생성

---
+++ dags/dags_simple_http_operatpor.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.decorators import task 
+import pendulum
+
+with DAG(
+    dag_id='dags_simple_http_operator',
+    start_date=pendulum.datetime(2024, 3, 1, tz='Asia/Seoul'),
+    catchup=False,
+    schedule=None
+) as dag:
+    '''서울시 공공자전거 대여소 정보'''
+    tb_cycle_station_info = SimpleHttpOperator(
+        task_id='tb_cycle_station_info',
+        http_conn_id='openapi.seoul.go.kr',
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/tbCyclestationInfo/1/10',
+        method='GET',
+        headers={'Content-Type': 'application/json',
+                 'charset':'utf-8',
+                 'Accept':'*/*'
+        }
+    )
+
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull(task_ids='tb_cycle_station_info')
+        import json
+        from pprint import pprint
+        pprint(json.loads(rslt))
+
+    pull_result = PythonOperator(
+        task_id='pull_result',
+        python_callable=python_2
+
+    )
+
+    tb_cycle_station_info >> pull_result
